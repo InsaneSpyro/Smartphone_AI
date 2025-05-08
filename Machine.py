eea99c490f76cb8eba382ac6775456016921629a
Jonas Brueggemann <jonas.brueggemann@perfact.de>
2025-05-08 14:22:36 +0200

    Use of classes for the nodes

--- Machine.py
+++ Machine.py
@@ -1,7 +1,23 @@
 def main():
-    # Knoten definieren
-    Alter = ["jung", "alt"]
-    Einkommen = ["niedrig", "hoch"]
-    Kaufentscheidung = ["Samsung", "Apple"]
+    class age:
+        # Node for the age of the purchasing customer
+        def __init__(self, young, old):
+            self.young = young
+            self.old = old
+    
+    class salary:
+        # Node for the salary of the purchasing customer
+        def __init__(self, low, high):
+            self.low = low
+            self.high = high
+    
+    class purchase_decision:
+        # Node for the purchase decision of the customer
+        def __init__(self, samsung, apple):
+            self.samsung = samsung
+            self.apple = apple 
+
+
+
 
 
