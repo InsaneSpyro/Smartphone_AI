7c60f12e608b77f3bb0892c063d503a58ab8ca9e
Jonas Brueggemann <jonas.brueggemann@perfact.de>
2025-05-15 10:46:38 +0200

    Add comments

--- Machine.py
+++ Machine.py
@@ -3,13 +3,12 @@ import pandas as pd
 
 df = pd.read_excel("Trainingsdaten.xlsx", engine="openpyxl")
 
-def main():
-    class customer:
-        # Node for the customer type and their purchase decision
-        def __init__(self, age, salary, purchase_decision):
-            self.age = age 
-            self.salary = salary 
-            self.purchase_decision = purchase_decision
+class customer:
+    # Node for the customer type and their purchase decision
+    def __init__(self, age, salary, purchase_decision):
+        self.age = age # (Jung, Alt)
+        self.salary = salary # (Niedrig, Hoch)
+        self.purchase_decision = purchase_decision # (Samsung, Apple)
     
 
 
