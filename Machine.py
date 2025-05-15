a316b39fce1de3eb7fe6c6c9e753626652a46270
Jonas Brueggemann <jonas.brueggemann@perfact.de>
2025-05-15 10:56:22 +0200

    Make columns from excel file Python friendly and embellishments of params names

--- Machine.py
+++ Machine.py
@@ -1,14 +1,22 @@
 # Import pandas libary to read the excel file with training data
 import pandas as pd
 
+# Read Excel file 
 df = pd.read_excel("Trainingsdaten.xlsx", engine="openpyxl")
 
+# Make column names Python code friendly
+df.columns = ["Einkommen", "Alter", "Kaufentscheidung"]
+df["Einkommen"] = df["Einkommen"].map({1: "Hoch", 0: "Niedrig"})
+df["Alter"] = df["Alter"].map({1: "Jung", 0: "Alt"})
+df["Kaufentscheidung"] = df["Kaufentscheidung"].map({1: "Apple", 0: "Samsung"})
+
+# Customer class with probabilities
 class customer:
     # Node for the customer type and their purchase decision
-    def __init__(self, age, salary, purchase_decision):
-        self.age = age # (Jung, Alt)
-        self.salary = salary # (Niedrig, Hoch)
-        self.purchase_decision = purchase_decision # (Samsung, Apple)
+    def __init__(self, age_prob, salary_prob, purchase_decision_prob):
+        self.salary_prob = salary_prob # (Niedrig, Hoch)
+        self.age_prob = age_prob # (Jung, Alt)
+        self.purchase_decision_prob = purchase_decision_prob # (Samsung, Apple)
     
 
 
